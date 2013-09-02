03e599cb3c21f37448fe9ee1b21b8748a77f8e3b
system <tamannagarg1201@gmail.com>
2013-09-02 21:30:38 +0530

    Update Gemfile.lock for Heroku

--- Gemfile
+++ Gemfile
@@ -31,6 +31,11 @@ group :doc do
   gem 'sdoc', '0.3.20', require: false
 end
 
+group :production do
+  gem 'pg', '0.15.0'
+  gem 'rails_12factor', '0.0.2'
+end
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/boot.rb
+++ config/boot.rb
@@ -2,5 +2,5 @@ require 'rubygems'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
-
+ENV['NLS_LANG'] = 'AMERICAN_AMERICA.WE8MSWIN1252'
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
